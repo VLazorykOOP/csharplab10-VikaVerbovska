7f74ceee57bd0b6c1bb1020e392d6e784d796408
Victoria-PC <Victoria-PC@DESKTOP-96B06D2>
2025-05-21 21:25:39 +0300

    lab10

--- Lab9_10CharpT/Program.cs
+++ Lab9_10CharpT/Program.cs
@@ -1,8 +1,138 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Lab#9  or  Lab#10");
-//  За бажанням студента для задач можна створювати консольний проект або WinForm
-// Бажано для задач лаб. робіт створити окремі класи
-// Виконання  виконати в стилі багатозаданості :
-//   Lab9T2  lab9task2 = new Lab9T2; lab9task2.Run();
-// При бажанні можна створити багатозадачний режим виконання задач.
+﻿using System;
+using System.Collections.Generic;
 
+namespace StudentLifeSimulation
+{
+    public delegate void StudentEventHandler(object sender, StudentEventArgs e);
+
+    public class StudentLife
+    {
+        public event StudentEventHandler StudentEvent;
+
+        private string name;
+        private int days;
+        private Random rnd = new Random();
+        private string[] events = { "Lecture", "Test", "Rest", "Exam", "Coursework" };
+
+        public StudentLife(string name, int days)
+        {
+            this.name = name;
+            this.days = days;
+
+            // Підписка сервісів
+            new Teacher(this).Subscribe();
+            new Friend(this).Subscribe();
+            new DeanOffice(this).Subscribe();
+        }
+
+        public void StartLife()
+        {
+            for (int day = 1; day <= days; day++)
+            {
+                Console.WriteLine($"\n--- День {day} ---");
+
+                string currentEvent = events[rnd.Next(events.Length)];
+                StudentEventArgs e = new StudentEventArgs(currentEvent, day);
+
+                OnStudentEvent(e);
+            }
+        }
+
+        protected virtual void OnStudentEvent(StudentEventArgs e)
+        {
+            if (StudentEvent != null)
+            {
+                foreach (StudentEventHandler handler in StudentEvent.GetInvocationList())
+                {
+                    handler(this, e);
+                    if (!string.IsNullOrEmpty(e.Response))
+                        Console.WriteLine(" -> " + e.Response);
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Немає підписників на подію.");
+            }
+        }
+    }
+
+    public class StudentEventArgs : EventArgs
+    {
+        public string EventName { get; }
+        public int Day { get; }
+        public string Response { get; set; }
+
+        public StudentEventArgs(string eventName, int day)
+        {
+            EventName = eventName;
+            Day = day;
+        }
+    }
+
+    public abstract class StudentService
+    {
+        protected StudentLife life;
+        protected Random rnd = new Random();
+
+        public StudentService(StudentLife life)
+        {
+            this.life = life;
+        }
+
+        public void Subscribe()
+        {
+            life.StudentEvent += HandleEvent;
+        }
+
+        public abstract void HandleEvent(object sender, StudentEventArgs e);
+    }
+
+    public class Teacher : StudentService
+    {
+        public Teacher(StudentLife life) : base(life) { }
+
+        public override void HandleEvent(object sender, StudentEventArgs e)
+        {
+            if (e.EventName == "Lecture" || e.EventName == "Test")
+            {
+                e.Response = $"Викладач провів {e.EventName.ToLower()}.";
+            }
+        }
+    }
+
+    public class Friend : StudentService
+    {
+        public Friend(StudentLife life) : base(life) { }
+
+        public override void HandleEvent(object sender, StudentEventArgs e)
+        {
+            if (e.EventName == "Rest")
+            {
+                e.Response = "Друг запросив пограти в комп'ютерні ігри.";
+            }
+        }
+    }
+
+    public class DeanOffice : StudentService
+    {
+        public DeanOffice(StudentLife life) : base(life) { }
+
+        public override void HandleEvent(object sender, StudentEventArgs e)
+        {
+            if (e.EventName == "Exam" || e.EventName == "Coursework")
+            {
+                e.Response = $"Деканат зареєстрував {e.EventName.ToLower()} студента.";
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentLife life = new StudentLife("Іван", 10);
+            life.StartLife();
+        }
+    }
+}
